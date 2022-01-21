3f1615ebf7db34d12d31dd20693ce94b8de0200a
Alexandr Belkin <msveet@gmail.com>
2022-01-22 06:59:18 +0700

    belkin problems/1 (1630. Arithmetic Subarrays)

--- problems/1 (1630. Arithmetic Subarrays)/belkin.js
+++ problems/1 (1630. Arithmetic Subarrays)/belkin.js
@@ -1,21 +1,70 @@
 /**
- * Для начала попробуем в лоб (Не готово, сохраню, потом доделаю)
+ * Для начала попробуем в лоб
+ * 144 ms ms 45.5 MB, faster than 98.36%
+ * Хотя это лучший, другие запуски были медленнее, иногда раза в полтора.
  */
-var checkArithmeticSubarrays = function(nums, l, r) {
+var checkArithmeticSubarrays1 = function(nums, l, r) {
 	let answer = [];
 	for (let i = 0; i < l.length; i++) {
-		if (l[i] - r[i] < 2) {
+		if (r[i] - l[i] < 2) {
 			answer.push(true);
+			continue;
 		}
-		let temp = nums.slice(l[i], r[i]).sort;
+		let temp = nums.slice(l[i], r[i] + 1);
+		temp.sort((a ,b) => a - b);
 		let diff = temp[1] - temp[0];
-		for (let j = 0; j < temp.length; j++) {
-
+		let arithmetic = true;
+		for (let j = 0; j < temp.length - 1; j++) {
+			if (temp[j + 1] - temp[j] !== diff) {
+				arithmetic = false;
+				break;
+			}
 		}
+		answer.push(arithmetic);
 	}
 	return answer;
 };
 
-let nums = [4,6,5,9,3,7], l = [0,0,2], r = [2,3,5];
+/**
+ * Runtime: 80 ms, faster than 100.00%
+ * Взял твой вариант и заюзал массив вместо Set.
+ * Отрабатывал стабильно быстрее. Прикол, конечно.
+ * Я думаю, что у тебя Set.add добавлет достаточно много времени
+ * внутри первого цикла, что немного обесценивает быстрый has().
+ * Так что думаю, что на больших миллионных суб-массивах медленный includes
+ * добавлял бы времени.
+ * Но хотя тогда и Set.add() замедлялся бы..... вопрос оставляю для себя открытым)
+ */
+var checkArithmeticSubarrays = function(nums, l, r) {
+	let result = []
+	for (let i = 0; i < l.length; i++) {
+		let min = nums[l[i]]
+		let max = nums[l[i]]
+		let count = 0
+		let sum = 0
+		let set = []
+		for (let j = l[i]; j <= r[i]; j++) {
+			count++;
+			sum += nums[j]
+			min = min > nums[j] ? nums[j] : min;
+			max = max > nums[j] ? max: nums[j];
+			set.push(nums[j])
+		}
+		let d = (max - min)/(count - 1);
+		let possible = true;
+		for (let z = 1; z < set.length; z++) {
+			if (!set.includes(min + z*d)) {
+				possible = false;
+				break;
+			}
+		}
+		result.push(possible)
+	}
+	return result;
+};
+
+let nums = [-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10],
+	l = [0, 1, 6, 4, 8, 7],
+	r = [4, 4, 9, 7, 9, 10];
 
 console.log(checkArithmeticSubarrays(nums, l, r));
