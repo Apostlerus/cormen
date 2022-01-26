c69fff9569cdb7dab6bf6f665febbc4683040c6c
Alexandr Belkin <msveet@gmail.com>
2022-01-26 13:51:59 +0700

    belkin problems/3 (179. Largest Number)

--- problems/3 (179. Largest Number)/belkin.js
+++ problems/3 (179. Largest Number)/belkin.js
@@ -1,20 +1,25 @@
 /**
- * начал, не готово
+ * Мда, субботняя идея с навороченной функцией сортировки была не очень.
+ * Хотел там как-то сравнивать порядок, приводить к одному порядку,
+ * рассматривать разные варианты отношений пар.....
+ * Короче наворотил еще не много, а тесты уже ломались,
+ * понял что сильно избыточно выходит.
+ *
+ * Теперь так:
+ * 80 ms, faster than 78.45%
+ * Не знаю, может можно ли ускорить, избавившись как-то от парсинга больших интов.
  */
 var largestNumber = function(nums) {
   nums.sort((a, b) => {
-    let alength = a.toString().length;
-    let blength = b.toString().length;
-    let diff = Math.abs(alength - blength);
-    if (diff === 0) {
-      return b - a;
-    }
-    if (alength > blength) {
-
-    }
+    return parseInt([b, a].join('')) - parseInt([a, b].join(''));
   });
+  let res = nums.join('');
+  if (parseInt(res) === 0) {
+    return '0';
+  }
+  return nums.join('');
 };
 
-let nums = [10,2];
+let nums = [2, 10];
 
 console.log(largestNumber(nums));
