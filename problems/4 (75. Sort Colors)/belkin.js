34f89ea06eed9a1459cc3e9590a2a5933cb71384
Alexandr Belkin <msveet@gmail.com>
2022-01-26 19:56:38 +0700

    belkin problems/4 (75. Sort Colors)

--- problems/4 (75. Sort Colors)/belkin.js
+++ problems/4 (75. Sort Colors)/belkin.js
@@ -3,7 +3,7 @@
  * Хоть сложность O(n), но прохода - два.
  * Надо еще подумать, как в предложенный один проход уложиться.
  */
-var sortColors = function(nums) {
+var sortColors2 = function(nums) {
   let counts = [0, 0, 0];
   for (let i = 0; i < nums.length; i++) {
     counts[nums[i]]++;
@@ -17,6 +17,30 @@ var sortColors = function(nums) {
   }
 };
 
-let nums = [2,0,2,1,1,0];
+/**
+ *  68 ms, faster than 94.53%
+ *  Один хрен. Можно одним проходом "выталкивать" нули вниз и двойки вверх,
+ *  пододвигая "края" к середине, пока не дойдешь до верхнего "края".
+ *  Но количество операций внутри цикла всё равно больше, так что и время то же.
+ *  Хотя у меня уже подозрение что есть какой-то минимум, меньше которого
+ *  на литкоде не покажет никогда) Вот были бы бОльшие ряды....
+ */
+var sortColors = function(nums) {
+  let left = 0, mid = 0, right = nums.length - 1;
+  while(mid <= right) {
+    if (nums[mid] === 2) {
+      [nums[mid], nums[right]] = [nums[right], nums[mid]];
+      right--;
+    } else if (nums[mid] === 0) {
+      [nums[mid], nums[left]] = [nums[left], nums[mid]];
+      mid++;
+      left++;
+    } else {
+      mid++;
+    }
+  }
+};
+
+let nums = [2,0,1];
 sortColors(nums)
 console.log(nums);
