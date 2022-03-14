c94378dda03bbecd3211c2978ebada0e718c26df
Alexandr Belkin <msveet@gmail.com>
2022-03-14 18:22:12 +0700

    belkin problems/17 (69. Sqrt(x))

--- problems/17 (69. Sqrt(x))/belkin.js
+++ problems/17 (69. Sqrt(x))/belkin.js
@@ -16,5 +16,32 @@ var mySqrt = function(x) {
   return res - 1;
 };
 
-let x = 15;
-console.log(mySqrt(x));
+/**
+ * 76 ms, faster than 92.46%
+ * Ок, по матеше не придумал красивых решений. Каюсь, подсмотрел, что можно же
+ * поиск сделать двоичным. Стало несколько быстрее.
+ */
+var mySqrt2 = function(x) {
+  if (x === 0) {
+    return 0;
+  }
+  if (x < 4) {
+    return 1;
+  }
+  let min = 2, max = 92681;
+  while (max - min > 1) {
+    let mid = Math.floor((max + min) / 2);
+    if (mid * mid === x) {
+      return mid;
+    }
+    if (mid * mid > x) {
+      max = mid;
+    } else {
+      min = mid;
+    }
+  }
+  return min;
+};
+
+let x = 8;
+console.log(mySqrt2(x));
