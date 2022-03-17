3eac6342d171a4caa91e3cb736bd3a2893fc6526
Alexandr Belkin <msveet@gmail.com>
2022-03-17 12:41:14 +0700

    belkin problems/23 (1877. Minimize Maximum Pair Sum in Array)

--- problems/23 (1877. Minimize Maximum Pair Sum in Array)/belkin.js
+++ problems/23 (1877. Minimize Maximum Pair Sum in Array)/belkin.js
@@ -28,5 +28,24 @@ var minPairSum2 = function(nums) {
   return res;
 };
 
+/**
+ * 308 ms, faster than 96.89%
+ * o_O Да что с этой задачей не так...
+ * Избавился от shift, стало значительно быстрее. Но все равно не понимаю, откуда
+ * такое время.
+ */
+var minPairSum3 = function(nums) {
+  let sums = new Set(), i = 0, j = nums.length - 1, res = Number.MIN_SAFE_INTEGER;
+  nums.sort((a, b) => a - b);
+  while (i < j) {
+    sums.add(nums[i++] + nums[j--]);
+  }
+  sums = [...sums];
+  for (let i = 0; i < sums.length; i++) {
+    res = Math.max(res, sums[i]);
+  }
+  return res;
+};
+
 let nums = [3,5,4,2,4,6];
-console.log(minPairSum2(nums));
+console.log(minPairSum3(nums));
