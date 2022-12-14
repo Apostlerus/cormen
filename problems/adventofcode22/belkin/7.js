4acea85f581b227f12138d3a44876e6f611d6413
Alexandr Belkin <msveet@gmail.com>
2022-12-14 17:26:15 +0700

    belkin adnventofcode22/7-1

---
+++ problems/adventofcode22/belkin/7.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const allFileContents = fs.readFileSync('7.input', 'utf-8');
+let t = {};
+let f = [];
+allFileContents.split(/\r?\n/).forEach(line =>  {
+  line = line.split(' ');
+  if (line[0] === '$') {
+    if (line[1] === 'cd') {
+      if (line[2] === '/') {
+        f = [];
+      } else if (line[2] === '..') {
+        f.pop();
+      } else {
+        f.push(line[2]);
+      }
+    }
+  } else {
+    if (!isNaN(line[0])) {
+      let fn = '';
+      for (let i = 0; i < f.length; i++) {
+        fn += '/' + f[i];
+        let size = parseInt(line[0]);
+        t[fn] = t[fn] + size || size;
+      }
+    }
+  }
+});
+let nums = Object.values(t);
+let res = 0;
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] <= 100000) {
+    res += nums[i];
+  }
+}
+
+console.log(res);
